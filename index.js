53fa317f890cbaa27c1d6cce10bbd12c7d89bb2b
ac1989 <welladjusted220@gmail.com>
2018-04-17 19:34:01 +0100

    passport google

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+require('./services/googleAuth');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ status: 'OK!' });
 });
+
+require('./routes/auth')(app);
 
 // Deployment 01; Get PORT from Heroku env, else use 5000
 const PORT = process.env.PORT || 5000;
---
+++ services/googleAuth.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const keys = require('../config/keys');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    token => console.log(token)
+  )
+);
